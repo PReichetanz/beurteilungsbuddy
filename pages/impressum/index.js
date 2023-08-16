e3df60ed1826fd9874d37cb8f6c149aee866b3d4
PReichetanz <33567019+PReichetanz@users.noreply.github.com>
2023-08-16 20:28:55 +0200

    add basic impressum page

---
+++ pages/impressum/index.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout";
+
+export default function ImpressumPage() {
+  return (
+    <Layout>
+      <h2>Impressum</h2>
+      <p>TBC when going live</p>
+    </Layout>
+  );
+}
